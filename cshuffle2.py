098b578b177749c1ff0908f945ce21601fd12f82
Marcus Larsen <marcuslarsen97@gmail.com>
2017-10-17 14:19:56 +0200

    Add function main() and support for cmd line args

--- cshuffle2.py
+++ cshuffle2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
+
+
 def newdeck(suits=4, cards=13):
     # cards = cards per suit
     alph = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
@@ -46,15 +49,14 @@ def find_period(deck, ceiling=1000):
 
     # if no match was found, return 0
     return 0
-    
 
-if __name__ == '__main__':
-    s_min = 1 # s er antal kulører
-    s_max = 10
-    n_min = 10 # n er antal kort i hver kulør
-    n_max = 40
-    ceiling = 1000 # loftet for find_period()
 
+def printhelp():
+    print('Usage: cshuffle2.py s_min s_max n_min n_max ceiling')
+    print('all args must be integers')
+
+
+def main(s_min, s_max, n_min, n_max, ceiling):
     try:
         for s in range(s_min, s_max+1): # +1 fordi range er ekskl.
             for n in range(n_min, n_max+1): # +1 fordi range er ekskl.
@@ -66,3 +68,22 @@ if __name__ == '__main__':
                 print('[%i, %i, %i, %i]' % (s, n, (s*n), p))
     except KeyboardInterrupt:
         print()
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:] # read arguments
+    if not args or args[0] == '--help' or len(args) != 5:
+        printhelp()
+        pass
+    else:
+        try:
+            s_min = int(args[0]) # s er antal kulører
+            s_max = int(args[1])
+            n_min = int(args[2]) # n er antal kort i hver kulør
+            n_max = int(args[3])
+            ceiling = int(args[4]) # loftet for find_period()
+        except ValueError:
+            print('Error: must be integers.')
+            printhelp()
+
+        main(s_min, s_max, n_min, n_max, ceiling)
